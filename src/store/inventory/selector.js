97ccf4066ca073aa7ec5ed96fc9f8dd4615521cd
curtis-clement <curtis.clement.nl@gmail.com>
2020-04-07 11:05:19 +0200

    add inventory selectory/reducer, update rootReudcer

---
+++ src/store/inventory/selector.js
@@ -0,0 +1,5 @@
+export function selectAllInventory(reduxState) {
+  console.log('Hello, I select the inventory')
+  // return reduxState.cars
+  // not sure what reduxState should reference --- update this once database is complete
+}
