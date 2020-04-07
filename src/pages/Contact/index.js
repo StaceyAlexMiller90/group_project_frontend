b169e93b1a6f312849255d41b5de3a63648517fd
curtis-clement <curtis.clement.nl@gmail.com>
2020-04-07 14:10:51 +0200

    Add contact and home page dummy info

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Contact() {
   return (
     <div>
-      Contact Information
+      <h1>Contact Information</h1>
+      <br></br>
+      <h2>Please contact us with any information or if you would like to schedule a test drive!</h2>
+      <ul>
+        <li>days open/hours of operation again?</li>
+        <li>phone number</li>
+        <li>dealership email</li>
+        <li>could have links to facebook and instagram as well here?</li>
+      </ul>
     </div>
   )
 }
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Homepage() {
   return (
     <div>
-      Homepage Test
+      <header>Welcome to our Dealership!</header>
+      <br></br>
+      <h2>The rest of this part can just be basic information</h2>
+      <br></br>
+      <ul>
+        <li>A slogan?</li>
+        <li>hours of operation?</li>
+        <li>a background image</li>
+        <li>fake link to instagram and facebook</li>
+      </ul>
     </div>
   )
 }
