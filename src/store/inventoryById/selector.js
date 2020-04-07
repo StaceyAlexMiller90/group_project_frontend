b3403b759bcc77c271891bda381f82e9055f88b2
curtis-clement <curtis.clement.nl@gmail.com>
2020-04-07 11:26:23 +0200

    add selector/reduce for inventoryById

---
+++ src/store/inventoryById/selector.js
@@ -0,0 +1,5 @@
+export function selectInventoryById (reduxState) {
+  console.log('Helo, I select invenotry by Id')
+  //return reduxState.carDetails;
+  // not sure what reduxState should reference --- update this once database is complete
+}
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import appState from "./appState/reducer";
 import user from "./user/reducer";
 import inventory from './inventory/reducer';
+import inventoryById from './inventoryById/reducer';
 
 export default combineReducers({
   appState,
   user,
-  inventory
+  inventory,
+  inventoryById
 });
