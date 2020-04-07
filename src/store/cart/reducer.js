f0e88681bc977fda9d0d7f268b62d980eba4573e
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-04-07 17:48:32 +0200

    added cart to local storage

--- src/store/cart/reducer.js
+++ src/store/cart/reducer.js
@@ -1,10 +1,12 @@
 import {ADD_PRODUCT_TO_CART} from './action';
 
-const initialState = []
+
+const initialState = JSON.parse(localStorage.getItem("cart")) || []
 
 export default (state = initialState, action) => {
   switch(action.type) {
     case ADD_PRODUCT_TO_CART:
+      localStorage.setItem("cart", JSON.stringify([...state, ...action.payload]));
       return [...state, ...action.payload]
 
     default:
