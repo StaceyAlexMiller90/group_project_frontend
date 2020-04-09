b8a57cdd5e22a957f0887db38666e5494b1426c5
uozgan <utkuozgan_8@hotmail.com>
2020-04-09 12:28:20 +0200

    Functional button added to sort by price

--- src/pages/Inventory/index.js
+++ src/pages/Inventory/index.js
@@ -1,38 +1,68 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Loading from '../../components/Loading'
-import { fetchAllInventory } from '../../store/inventory/action'
-import { selectAllInventory } from '../../store/inventory/selector'
-import InventoryCard from '../../components/InventoryCard'
+import Loading from "../../components/Loading";
+import { fetchAllInventory } from "../../store/inventory/action";
+import { selectAllInventory } from "../../store/inventory/selector";
+import InventoryCard from "../../components/InventoryCard";
+import Button from "react-bootstrap/Button";
+
+function comparePrice(car_a, car_b) {
+  return car_b.price - car_a.price;
+}
 
 export default function Inventory() {
-  const dispatch = useDispatch()
-  const inventory = useSelector(selectAllInventory)
+  const dispatch = useDispatch();
+  const inventory = useSelector(selectAllInventory);
+  const [sortedCars, setSortedCars] = useState(true);
 
   useEffect(() => {
-    if(inventory.length === 0) {
-      dispatch(fetchAllInventory())
+    if (inventory.length === 0) {
+      dispatch(fetchAllInventory());
     }
-  }, [])
+  }, []);
+
+  const sortCars = () => {
+    setSortedCars(!sortedCars);
+  };
+  const sortedInventory = inventory
+    ? inventory.sort(comparePrice).reverse()
+    : [];
 
   if (!inventory) {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
-    <>  
+    <>
       <h1>All of our inventory</h1>
-      {inventory.map((car, i) => {
-        return <InventoryCard 
-               key={i}
-               id={car.id}
-               type={car.type}
-               brand={car.brand}
-               price={car.price}
-               model={car.model}
-               imageUrl={car.imageUrl}
-               />
-      })}
+      <Button onClick={sortCars}>Sort by Price</Button>
+      {sortedCars
+        ? sortedInventory.map((car, i) => {
+            return (
+              <InventoryCard
+                key={i}
+                id={car.id}
+                type={car.type}
+                brand={car.brand}
+                price={car.price}
+                model={car.model}
+                imageUrl={car.imageUrl}
+              />
+            );
+          })
+        : sortedInventory.reverse().map((car, i) => {
+            return (
+              <InventoryCard
+                key={i}
+                id={car.id}
+                type={car.type}
+                brand={car.brand}
+                price={car.price}
+                model={car.model}
+                imageUrl={car.imageUrl}
+              />
+            );
+          })}
     </>
-    )
+  );
 }
